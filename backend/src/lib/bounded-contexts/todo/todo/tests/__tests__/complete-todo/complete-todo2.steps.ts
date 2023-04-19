36fdbb5b5f1b809026e51021ab40e6c8d646ab6e
sardounis <ant.sardounis@gmail.com>
2023-04-19 17:37:29 +0300

    finalized the tests and refactored the test code

--- backend/src/lib/bounded-contexts/todo/todo/tests/__tests__/complete-todo/complete-todo2.steps.ts
+++ backend/src/lib/bounded-contexts/todo/todo/tests/__tests__/complete-todo/complete-todo2.steps.ts
@@ -10,10 +10,14 @@ import { mockAsyncLocalStorageGet } from '../../mocks/mockAsynLocalStorageGet.mo
 import { ContextBuilder } from '../../builders/context.builder';
 import { MockTodoWriteRepo } from './todo-write-repo.mock';
 
-describe('Complete todo feature test', () => {
-  it('Todo completed successfully', async () => {
+describe('Feature: Complete todo feature test', () => {
+  it(`Scenario: Todo is completed successfully:
+    Given that a valid todo exists and it is not completed
+    When a complete todo command is issued
+    Then the todo should be persisted as completed 
+    And a todo completed domain event should be published 
+    And the result should be ok`, async () => {
     // given
-    console.log(`given that a valid todo exists and is not completed`);
 
     const COMPLETE_TODO_SUCCESS_PRIMITIVES = {
       userId: { id: '1234' },
@@ -50,9 +54,6 @@ describe('Complete todo feature test', () => {
     const result = await completeTodoHandler.execute(completeTodoCommand);
 
     //then
-    console.log(
-      `Then the todo should be persisted as completed and a todo completed domain event should be published and the result should be ok`,
-    );
     const todoProps = new TodoPropsBuilder()
       .withTitle(COMPLETE_TODO_SUCCESS_PRIMITIVES.title.title)
       .withCompleted(true)
@@ -75,10 +76,13 @@ describe('Complete todo feature test', () => {
     expect(result.value).toBeUndefined();
   });
 
-  it('Todo completed failed, todo not found', async () => {
+  it(`Scenario: Todo completed failed, since the todo was not found:
+      Given that a todo does not exists
+      When a complete todo command is issued
+      Then the todo should not be persisted 
+      And a todo not found error should be returned
+    `, async () => {
     // given
-    console.log(`given that the todo exists and is not completed`);
-
     const COMPLETE_TODO_NOT_FOUND_CASE = {
       userId: { id: '123' },
       id: 'todo2',
@@ -100,25 +104,24 @@ describe('Complete todo feature test', () => {
     });
 
     // when
-    console.log(`When a complete todo command is executed`);
     const completeTodoHandler = new CompleteTodoHandler(
       mockTodoWriteRepo.getMockTodoWriteRepo(),
     );
     const result = await completeTodoHandler.execute(completeTodoCommand);
 
     //then
-    console.log(
-      `Then the todo should not be persisted and a todo not found error should be returned`,
-    );
     expect(mockTodoWriteRepo.mockGetByIdMethod).toHaveBeenCalledWith(
       new Domain.UUIDv4(COMPLETE_TODO_NOT_FOUND_CASE.id),
     );
     expect(result.value).toBeInstanceOf(ApplicationErrors.TodoNotFoundError);
   });
 
-  it('Todo completed failed, todo already completed', async () => {
+  it(`Scenario: Todo completed failed because the todo was already completed
+      Given that the todo exists and it is completed
+      When a complete todo command is issued
+      Then the todo should not be persisted 
+      And a todo was already completed error should be issued`, async () => {
     // given
-    console.log(`given that the todo exists and is not completed`);
     const COMPLETE_TODO_ALREADY_COMPLETED_PRIMITIVES = {
       userId: { id: '123' },
       id: 'todo5',
@@ -155,7 +158,12 @@ describe('Complete todo feature test', () => {
     expect(result.value).toBeInstanceOf(DomainErrors.TodoAlreadyCompletedError);
   });
 
-  it('Todo failed to be completed, getById repo error', async () => {
+  it(`Scenario: Todo failed to be completed, due to an unexpected repository error
+    Given that a valid todo exists which is not completed
+    When a complete todo command is issued
+    Then the todo should not be persisted
+    And an unexpected repository error should be returned
+  `, async () => {
     console.log(`given that the todo exists and is not completed`);
     const COMPLETE_TODO_REPO_ERROR_GETBYID_PRIMITIVES = {
       userId: { id: '123' },
@@ -193,7 +201,12 @@ describe('Complete todo feature test', () => {
     expect(result.value).toBeInstanceOf(Application.Repo.Errors.Unexpected);
   });
 
-  it('Todo failed to be completed, save repo error', async () => {
+  it(`Scenario: Todo failed to be completed, because of an unexpected repository error
+      Given that a valid todo exists which is not completed
+      When a complete todo command is issued
+      Then the todo should not be persisted
+      And an unexpected repository error should be returned
+    `, async () => {
     const COMPLETE_TODO_REPO_ERROR_SAVE_PRIMITIVES = {
       userId: { id: '123' },
       id: 'todo4',
@@ -239,23 +252,11 @@ describe('Complete todo feature test', () => {
     const result = await completeTodoHandler.execute(completeTodoCommand);
 
     //then
-    const todoProps = new TodoPropsBuilder()
-      .withTitle(COMPLETE_TODO_REPO_ERROR_SAVE_PRIMITIVES.title.title)
-      .withCompleted(true)
-      .withUserId(COMPLETE_TODO_REPO_ERROR_SAVE_PRIMITIVES.userId.id)
-      .withId(COMPLETE_TODO_REPO_ERROR_SAVE_PRIMITIVES.id)
-      .build();
-
     expect(mockTodoWriteRepo.mockGetByIdMethod).toHaveBeenCalledWith(
       new Domain.UUIDv4(COMPLETE_TODO_REPO_ERROR_SAVE_PRIMITIVES.id),
     );
     expect(mockTodoWriteRepo.mockUpdateMethod).toHaveBeenCalledWith(
       expect.any(TodoEntity),
-    );
-    const todoAggregate = mockTodoWriteRepo.mockUpdateMethod.mock.calls[0][0];
-    expect(todoAggregate.props).toEqual(todoProps);
-    expect(todoAggregate.domainEvents[0]).toBeInstanceOf(
-      TodoCompletedDomainEvent,
     );
     expect(result.value).toBeInstanceOf(Application.Repo.Errors.Unexpected);
   });
